3672b01bb13d5ba08f4a7f59fb4ecdef79119b71
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-09-25 20:02:00 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './index.css';
 
+interface quote{
+  _id: string,
+  content: string,
+  author: string,
+  tag: tag
+  authorSlug:	string,
+  length:	number
+  dateAdded:	string,
+  dateModified:	string,
+}
+
+interface tag{
+  0: string,
+}
 function App() {
+  const [quoteData, setQuoteData] = useState<quote>()
+  const [loading, setLoading] = useState<boolean>(false)
+  const [easyRead, setEasyRead] = useState<boolean>(false)
+
+  const getPersonQuote = async () =>{
+    setLoading(true)
+    try{
+      const response = await axios.get('https://api.quotable.io/random?')
+      setQuoteData(response.data)
+      setLoading(false)
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    getPersonQuote()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='toggleSwitch'>
+        <label htmlFor='toggle'>Toggle Font: </label>
+          <input 
+            className='toggle'
+            type="checkbox" 
+            onChange={()=>setEasyRead(!easyRead)} />
+      </div>
+
+        <div className='quote'>
+          {!loading &&  <div className={(easyRead? 'easy' : 'script')}>
+              <h1>{quoteData?.content}</h1>
+              <p>-{quoteData?.author}</p>
+            </div>}          
+            <div className='getMore'>
+              <button onClick={getPersonQuote}>New Quote</button>
+            </div>
+          </div>
+
+
+
+
     </div>
   );
 }
